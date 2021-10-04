0d010811b5307ebe4d6ae7292f214b0785e1dcb1
AnanthanSuthan <suthanlove.yk@gmail.com>
2021-10-04 20:27:24 +0530

    emp management

--- AngularWebApi/ITP_Collaborate_ALL-Parts-main/src/app/shared/payment.model.ts
+++
@@ -1,8 +0,0 @@
-export class Payment {
-  paymentId: number = 0;
-  total: number;
-  voucherId: number;
-  promotionId: number;
-  totalDiscount: number = 0;
-  subTotal: number = 0;
-}
--- AngularWebApi/ITP_Collaborate_ALL-Parts-main/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-
-
-export const environment = {
-  production: false,
-  apiURL : 'http://localhost:46568/api'
-};
-
-
-
